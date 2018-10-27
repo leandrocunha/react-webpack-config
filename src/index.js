84b2f4e6f0a26348190243d7bf0d830b22397020
mayconbalves <mayconmustaine@hotmail.com>
2018-10-27 13:56:59 -0300

    feat(*): create app js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-// this line is new
-// we now have some nice styles on our react app
-import "index.scss";
-let HelloWorld = () => {
-  return <h1>Hello there World!</h1>
-}
-ReactDOM.render(
-  <HelloWorld/>,
-  document.getElementById("root")
-);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../src/scripts/containers/App'
+import 'index.scss'
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ src/scripts/containers/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>React with webpack and babel 7</h1>
+  )
+}
+
+export default App
