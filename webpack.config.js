8d4239d12060c2326597cae76d773970621fbb89
mayconbalves <mayconmustaine@hotmail.com>
2018-10-26 22:59:32 -0300

    finish boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,47 @@
-import path from 'path'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 module.exports = {
-	// these are used to tell our server what has to be compiled and from where
-	entry: path.join(__dirname, 'src', 'index.js'),
-	// It also tells where to put the outputted compiled version
-	output: {
-		path: path.join(__dirname, 'build'),
-		filename: 'index.bundle.js'
-	},
-	// this is the mode of our output. We are setting it to ‘development’.
-	// If in the scripts we specify the NODE_ENV variable,
-	// it will take that one instead. See the below example on how to use NODE_ENV
-	// (note that the below changes will not be made inside the package.json file
-	// in this tutorial, it is just an example for you to see how it works)
-	mode: process.env.NODE_ENV || 'production',
-	// this is used so that we can import anything from src folder in relative
-	// paths instead of absolute ones. It is the same for the node_modules.
-	// We can import anything from node_modules directly without absolute paths
-	resolve: {
-		modules: [path.resolve(__dirname, 'src'), 'node_modules']
-	},
-	// this tells the webpack-dev-server what files are needed to be served.
-	// Everything from our src folder needs to be served (outputted) in the browser
-	devServer: {
-		contentBase: path.join(__dirname, 'src')
-	},
-	module: {
-	  rules: [
-	    {
-	      // this is so that we can compile any React,
-	      // ES6 and above into normal ES5 syntax
-	      test: /\.(js|jsx)$/,
-	      // we do not want anything from node_modules to be compiled
-	      exclude: /node_modules/,
-	      use: ['babel-loader']
-	    },
-	    {
-	      test: /\.(css|scss)$/,
-	      use: [
-		"style-loader", // creates style nodes from JS strings
-		"css-loader", // translates CSS into CommonJS
-		"sass-loader" // compiles Sass to CSS, using Node Sass by default
-	      ]
-	    },
-	    {
-	      test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-	      loaders: ['file-loader']
-	    }
-	  ]
-	},
-	// here we set what plugins we need in our app. As of this moment we only need
-	// the html-webpack-plugin which tells the server that the index.bundle.js
-	// should be injected (or added if you will) to our index.html file
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, 'src', 'index.html')
-		})
-	]
-}
+  entry: path.join(__dirname,'src','index.js'),
+  output: {
+    path: path.join(__dirname,'build'),
+    filename: 'index.bundle.js'
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname,'src')
+  },
+  module: {
+    rules: [
+      {
+        // this is so that we can compile any React,
+        // ES6 and above into normal ES5 syntax
+        test: /\.(js|jsx)$/,
+        // we do not want anything from node_modules to be compiled
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        loaders: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname,'src','index.html')
+    })
+  ]
+};
